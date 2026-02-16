f2c1ea0899e82186c27eb5c9adac653f5988cdef
unknown <teva.clairefond@laplateforme.io>
2026-02-16 11:19:33 +0100

    Correction activation.py et network.py

--- repo/activation.py
+++ repo/activation.py
@@ -5,10 +5,10 @@ def act_relu(x):
         return x
 
 def act_threshold(x):
-    if x < 0 :
-        return 0
-    else :
+    if x > 0 :
         return 1
+    else :
+        return 0
     
 def act_identity(x):
     return x
